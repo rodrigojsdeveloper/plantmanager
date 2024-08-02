ae1f3835c7081fdf255ad39142cde95152efc770
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-08-02 13:53:23 -0300

    style: style home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,25 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import wateringImg from '@/assets/watering.png'
+import colors from "@/styles/colors";
 
 export default function Index() {
   return (
-    <View
+    <SafeAreaView
       style={styles.container}
     >
-      <Text>Gerencie suas plantas de forma fácil</Text>
+      <Text style={styles.title}>Gerencie {'\n'}suas plantas de{'\n'} forma fácil</Text>
 
-      <Image source={wateringImg} />
+      <Image source={wateringImg} style={styles.image} />
 
-      <Text>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.</Text>
+      <Text style={styles.subtitle}>Não esqueça mais de regar suas{'\n'} plantas. Nós cuidamos de lembrar você{'\n'} sempre que precisar.</Text>
 
-      <TouchableOpacity>
-        <Text>
-          *
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>
+          Go
         </Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -26,5 +28,38 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: colors.white
+  },
+  title: {
+    fontWeight: '600',
+    fontSize: 32,
+    lineHeight: 38,
+    textAlign: 'center',
+    color: colors.heading,
+  },
+  image: {
+    width: 292.13,
+    height: 284.3,
+    marginVertical: 49
+  },
+  subtitle: {
+    fontWeight: '400',
+    fontSize: 17,
+    lineHeight: 25,
+    textAlign: 'center',
+    color: colors.heading,
+    marginBottom: 47
+  },
+  button: {
+    width: 56,
+    height: 56,
+    padding: 5,
+    backgroundColor: colors.green,
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: colors.white,
   }
 })
