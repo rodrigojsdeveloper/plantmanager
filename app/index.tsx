9ceb087faf909788137d3ee7da85e122b2bd797b
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-08-02 13:22:45 -0300

    feat: add home screen images and texts

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,30 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import wateringImg from '@/assets/watering.png'
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Hello World.</Text>
+      <Text>Gerencie suas plantas de forma fácil</Text>
+
+      <Image source={wateringImg} />
+
+      <Text>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar.</Text>
+
+      <TouchableOpacity>
+        <Text>
+          *
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
